1aeae8ec608443d57297bb6dba90374aa9cc1be4
Balancer <balancer@balancer.ru>
2017-12-17 21:30:34 +0300

    update

--- src/ZeroBlogStorage.php
+++ src/ZeroBlogStorage.php
@@ -16,10 +16,13 @@ class ZeroBlogStorage extends \B2\Obj
 		else
 			$object = new $class_name(NULL);
 
-		if(!preg_match('!/data/(\w{34})$!', $this->blog_dir(), $m))
+		if(!preg_match('!/data/(\w{33,34})$!', $this->blog_dir(), $m))
 			\B2\Exception::throw("Unknown ZeroID in blog_dir '%s'", $this->blog_dir());
 
 		$blog_zero_id = $m[1];
+
+		if(!file_exists($this->blog_dir().'/data/data.json'))
+			\B2\Exception::throw("Not exists file %s", $this->blog_dir().'/data/data.json');
 
 		$blog_data = json_decode(file_get_contents($this->blog_dir().'/data/data.json'));
 
@@ -33,13 +36,17 @@ class ZeroBlogStorage extends \B2\Obj
 		foreach($blog_data->post as $d)
 		{
 			$x = new ZeroBlogPost($d->post_id);
+			$x->set('blog', $blog);
 			$x->set('zero_id', $blog_zero_id);
 			$x->set('title', $d->title);
 			$x->set('create_time', round($d->date_published));
 
-			$text = $d->body;
+			$text = @$d->body;
 
 			$text = preg_replace("!\]\(data/img/post!", '](https://www.zerogate.tk/'.$blog_zero_id.'/data/img/post', $text);
+			$text = preg_replace("!\]\((/\w{33,34}/)!", '](https://www.zerogate.tk$1', $text);
+			$text = str_replace('http://127.0.0.1:43110/', 'https://www.zerogate.tk/', $text);
+			$text = preg_replace('!(^| )(https?://\S+?)( |$)!m', '$1<$2>$3', $text);
 
 			$x->set('source', $text);
 
